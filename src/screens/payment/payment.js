e5a64e6ebc6b75aa459e1e0011e95b5c0bd843e6
Dekoder6 <ekoder6@gmail.com>
2020-02-21 15:59:39 +0200

    fix filter

--- src/screens/payment/payment.js
+++ src/screens/payment/payment.js
@@ -18,144 +18,144 @@ export default class Payment extends Component {
   }
 
   isSelected(value) {
-      if(value === this.state.selected) {
-        return true
-      }
-      return false
+    if (value === this.state.selected) {
+      return true
+    }
+    return false
   }
 
   handlePayClick() {
-      switch (this.state.selected) {
-          case 'PayPalPlus':
-              this.payWithPayPal()
-              break;
-          case 'Ratenkauf':
-              alert('Ratenkauf')
-              break;
-          case 'AmazonPay':
-              alert('AmazonPay')
-              break;
-          case 'ApplePay':
-              this.payWithApplePay()
-              break;
-          default:
-              alert('SELECT PAYMENT METHOD')
-              break;
-      }
-      // this.props.navigation.navigate('OrderSuccess')
+    switch (this.state.selected) {
+      case 'PayPalPlus':
+        this.payWithPayPal()
+        break;
+      case 'Ratenkauf':
+        alert('Ratenkauf')
+        break;
+      case 'AmazonPay':
+        alert('AmazonPay')
+        break;
+      case 'ApplePay':
+        this.payWithApplePay()
+        break;
+      default:
+        alert('SELECT PAYMENT METHOD')
+        break;
+    }
+    // this.props.navigation.navigate('OrderSuccess')
   }
 
   payWithPayPal() {
-      this.setState({loading: true})
-      // let url = 'https://ptsv2.com/t/1rcni-1560428685/post';
-      const url = 'https://api.sandbox.paypal.com/v1/oauth2/token';
-      const username = 'AbkWPfPgd8Y2D9ZoJMuo0DROUxIAhcPW7yJxB4uyXzEJFH9ix8L5qHQxePbaXCvNv-EDLDRyx8m70eIX';
-      const password = 'EOj5ZbPxHqSSv2_xJWdgo5cWTesNYb-_W4zYV__WhTWYot-n41k-f1CRiZMcjBVFVwN-AJ9b6xROmXVb';
-      const authString = `${username}:${password}`
-      fetch(url, {
-          method: 'POST',
-          headers: {
-              'Authorization': 'Basic ' + btoa(authString),
-              'Accept': 'application/json',
-              'Content-Type': 'application/x-www-form-urlencoded'
-          },
-          body: "grant_type=client_credentials"
+    this.setState({ loading: true })
+    // let url = 'https://ptsv2.com/t/1rcni-1560428685/post';
+    const url = 'https://api.sandbox.paypal.com/v1/oauth2/token';
+    const username = 'AbkWPfPgd8Y2D9ZoJMuo0DROUxIAhcPW7yJxB4uyXzEJFH9ix8L5qHQxePbaXCvNv-EDLDRyx8m70eIX';
+    const password = 'EOj5ZbPxHqSSv2_xJWdgo5cWTesNYb-_W4zYV__WhTWYot-n41k-f1CRiZMcjBVFVwN-AJ9b6xROmXVb';
+    const authString = `${username}:${password}`
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Basic ' + btoa(authString),
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: "grant_type=client_credentials"
+    })
+      .then(response => {
+        if (response.ok) return response.json()
       })
       .then(response => {
-        if(response.ok) return response.json()
-      })
-      .then(response => {
-        console.log(response)
-        console.log(response.access_token)
+        console.log('resposne', response)
+        console.log('esponse.access_token', response.access_token)
         return response.access_token
       })
       .then(access_token => {
-          console.log('HERE');
-          const url = 'https://api.sandbox.paypal.com/v2/checkout/orders';
-          fetch(url, {
-              method: 'POST',
-              headers: {
-                  accept: 'application/json',
-                  'accept-language': 'en_US',
-                  'Authorization': 'Bearer ' + access_token,
-                  'Content-Type': 'application/json',
-                  // 'PayPal-Partner-Attribution-Id': 'ShopwareAG_Cart_PayPalPlus_1017'
-              },
-              body: JSON.stringify(
+        console.log('HERE');
+        const url = 'https://api.sandbox.paypal.com/v2/checkout/orders';
+        fetch(url, {
+          method: 'POST',
+          headers: {
+            accept: 'application/json',
+            'accept-language': 'en_US',
+            'Authorization': 'Bearer ' + access_token,
+            'Content-Type': 'application/json',
+            // 'PayPal-Partner-Attribution-Id': 'ShopwareAG_Cart_PayPalPlus_1017'
+          },
+          body: JSON.stringify(
+            {
+              intent: 'CAPTURE',
+              purchase_units: [
                 {
-                  intent: 'CAPTURE',
-                  purchase_units: [
+                  reference_id: 'store_mobile_world_order_1234',
+                  description: 'Mobile World Store order-1234',
+                  amount: {
+                    currency_code: 'USD',
+                    details: {
+                      subtotal: '1.09',
+                      shipping: '0.02',
+                      tax: '0.33'
+                    },
+                    value: '1.44'
+                  },
+                  payee: {
+                    email: 'seller@example.com'
+                  },
+                  items: [
                     {
-                      reference_id: 'store_mobile_world_order_1234',
-                      description: 'Mobile World Store order-1234',
-                      amount: {
-                        currency_code: 'USD',
-                        details: {
-                          subtotal: '1.09',
-                          shipping: '0.02',
-                          tax: '0.33'
-                        },
-                        value: '1.44'
-                      },
-                      payee: {
-                        email: 'seller@example.com'
-                      },
-                      items: [
-                        {
-                          name: 'NeoPhone',
-                          sku: 'sku03',
-                          price: '0.54',
-                          currency: 'USD',
-                          unit_amount: '1'
-                        },
-                        {
-                          name: 'Fitness Watch',
-                          sku: 'sku04',
-                          price: '0.55',
-                          currency: 'USD',
-                          unit_amount: '1'
-                        }
-                      ],
-                      shipping_address: {
-                        line1: '2211 N First Street',
-                        line2: 'Building 17',
-                        city: 'San Jose',
-                        country_code: 'US',
-                        postal_code: '95131',
-                        state: 'CA',
-                        phone: '(123) 456-7890'
-                      },
-                      shipping_method: 'United Postal Service',
-                      partner_fee_details: {
-                        receiver: {
-                          email: 'partner@example.com'
-                        },
-                        amount: {
-                          value: '0.01',
-                          currency: 'USD'
-                        }
-                      },
-                      payment_linked_group: 1,
-                      custom: 'custom_value_2388',
-                      invoice_number: 'invoice_number_2388',
-                      payment_descriptor: 'Payment Mobile World'
+                      name: 'NeoPhone',
+                      sku: 'sku03',
+                      price: '0.54',
+                      currency: 'USD',
+                      unit_amount: '1'
+                    },
+                    {
+                      name: 'Fitness Watch',
+                      sku: 'sku04',
+                      price: '0.55',
+                      currency: 'USD',
+                      unit_amount: '1'
                     }
                   ],
-                  application_context: {
-                    return_url: 'https://example.com/return',
-                    cancel_url: 'https://example.com/cancel'
-                  }
+                  shipping_address: {
+                    line1: '2211 N First Street',
+                    line2: 'Building 17',
+                    city: 'San Jose',
+                    country_code: 'US',
+                    postal_code: '95131',
+                    state: 'CA',
+                    phone: '(123) 456-7890'
+                  },
+                  shipping_method: 'United Postal Service',
+                  partner_fee_details: {
+                    receiver: {
+                      email: 'partner@example.com'
+                    },
+                    amount: {
+                      value: '0.01',
+                      currency: 'USD'
+                    }
+                  },
+                  payment_linked_group: 1,
+                  custom: 'custom_value_2388',
+                  invoice_number: 'invoice_number_2388',
+                  payment_descriptor: 'Payment Mobile World'
                 }
-              )
-          })
+              ],
+              application_context: {
+                return_url: 'https://example.com/return',
+                cancel_url: 'https://example.com/cancel'
+              }
+            }
+          )
+        })
           .then(res => res.json())
           .then(res => console.log(res))
           .catch(err => {
             console.log(err)
-        })
+          })
       })
       .catch(err => {
-          console.log({ ...err })
+        console.log({ ...err })
       })
   }
 
@@ -173,7 +173,7 @@ export default class Payment extends Component {
         currencyCode: 'EUR'
       }
     }];
-    const displayItems = deliveryPrice!=0 ? [
+    const displayItems = deliveryPrice != 0 ? [
       {
         label: 'Products',
         amount: { currency: 'EUR', value: productsPrice }
@@ -182,13 +182,13 @@ export default class Payment extends Component {
         label: 'Delivery',
         amount: { currency: 'EUR', value: deliveryPrice }
       }
-    ] : 
-    [
-      {
-        label: 'Products',
-        amount: { currency: 'EUR', value: productsPrice }
-      }
-    ]
+    ] :
+      [
+        {
+          label: 'Products',
+          amount: { currency: 'EUR', value: productsPrice }
+        }
+      ]
     const DETAILS = {
       id: 'basic-example',
       displayItems,
@@ -205,40 +205,40 @@ export default class Payment extends Component {
   }
 
   render() {
-    if(this.state.loading) return <Loading />
+    if (this.state.loading) return <Loading />
     console.log(this.state)
-    return(
-      <View style={{flex: 1}}>
-        <ScrollView style={{marginHorizontal: 18}}>
+    return (
+      <View style={{ flex: 1 }}>
+        <ScrollView style={{ marginHorizontal: 18 }}>
           <PaymentOption
-            onPress={() => this.setState({selected: 'PayPalPlus'})}
+            onPress={() => this.setState({ selected: 'PayPalPlus' })}
             selected={this.isSelected('PayPalPlus')}
             imageSource={require('../../assets/payments/PayPalPlus.png')}
             text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra mae'
           />
           <PaymentOption
-            onPress={() => this.setState({selected: 'Ratenkauf'})}
+            onPress={() => this.setState({ selected: 'Ratenkauf' })}
             selected={this.isSelected('Ratenkauf')}
             imageSource={require('../../assets/payments/Ratenkauf.png')}
             text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra mae'
           />
           <PaymentOption
-            onPress={() => this.setState({selected: 'AmazonPay'})}
+            onPress={() => this.setState({ selected: 'AmazonPay' })}
             selected={this.isSelected('AmazonPay')}
             imageSource={require('../../assets/payments/AmazonPay.png')}
             text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra mae'
           />
           {
-            Platform.OS === 'ios' ? 
-                <PaymentOption
-                    onPress={() => this.setState({selected: 'ApplePay'})}
-                    selected={this.isSelected('ApplePay')}
-                    imageSource={require('../../assets/payments/ApplePay.png')}
-                    text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra mae'
-                />
-            : null
+            Platform.OS === 'ios' ?
+              <PaymentOption
+                onPress={() => this.setState({ selected: 'ApplePay' })}
+                selected={this.isSelected('ApplePay')}
+                imageSource={require('../../assets/payments/ApplePay.png')}
+                text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra mae'
+              />
+              : null
           }
-          
+
         </ScrollView>
         <FooterButton text='Weiter' onPress={() => this.handlePayClick()} />
       </View>
