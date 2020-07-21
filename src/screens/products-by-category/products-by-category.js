a94516854b413e49d6593c0de3ac6acfa7bf0d5e
Snow <61739689+api-tester@users.noreply.github.com>
2020-07-21 17:48:20 +0300

    fix sync my code (Yevhen)

--- src/screens/products-by-category/products-by-category.js
+++ src/screens/products-by-category/products-by-category.js
@@ -18,43 +18,41 @@ import Loading from '../loading';
 
 import { connect } from 'react-redux'
 
+import FooterNavBar from '../../common/footer-navigation-bar/footer-navigation-bar';
+
 //export default class ProductsByCategory extends Component {
 class ProductsByCategory extends Component {
 
-    static navigationOptions = ({ navigation }) => {
-        const cmsText = navigation.getParam('cmsText', null)
-        const name = navigation.getParam('categoryName', 'Kategorien')
-        return {
-            title: name,
-            headerRight: (
-                <View style={{ flexDirection: 'row', marginRight: 9 }}>
-                    {/* {cmsText ? <CategoryInfoButton cmsText={cmsText} name={name} /> : null} */}
-                    <CategoryInfoButton cmsText={cmsText} name={name} />
-                    <SearchButton />
-                </View>
-            )
-        }
+  static navigationOptions = ({ navigation }) => {
+    const cmsText = navigation.getParam('cmsText', null);
+    const name = navigation.getParam('categoryName', 'Kategorien');
+    return {
+      title: name,
+      headerRight: (
+        <View style={{ flexDirection: 'row', marginRight: 9 }}>
+          {/* {cmsText ? <CategoryInfoButton cmsText={cmsText} name={name} /> : null} */}
+          <CategoryInfoButton cmsText={cmsText} name={name} />
+          <SearchButton />
+        </View>
+      )
     }
+  }
 
-    state = {
-        IDs: [],
-        data: [],
-        from: 0,
-        loaded: false
-    }
+  state = { IDs: [], data: [], from: 0, loaded: false }
 
-    getProductsIDs() {
-        // const id = 1
-        const id = this.props.navigation.getParam('categoryID', null)
-        getProductsByCategory(id)
-            .then(response => response.json())
-            .then(responseJson => {
-                this.setState({
-                    data: responseJson.products,
-                    IDs: responseJson.productsCount,
-                    loaded: true,
-                });
-            });
+  getProductsIDs() {
+    // const id = 1
+    const id = this.props.navigation.getParam('categoryID', null);
+    getProductsByCategory(id)
+      .then(response => response.json())
+      .then(responseJson => {
+        console.log('>>>>>', responseJson.products);
+        this.setState({
+          data: responseJson.products,
+          IDs: responseJson.productsCount,
+          loaded: true,
+        });
+      });
     }
 
     // getData(from) {
@@ -70,61 +68,73 @@ class ProductsByCategory extends Component {
     //     })
     // }
 
-    componentDidMount() {
-        this.getProductsIDs()
-    }
+  componentDidMount() {
+    this.getProductsIDs();
+  }
 
-    render() {
-        console.log("this.state RENDER() in products-by-category.js", this.state)
-        if (!this.state.loaded) {
-            return <Loading />
-        }
-        if (this.state.IDs.length < 1) {
-            return (
-                <View style={styles.noProductsContainer}>
-                    <Image source={require('../../assets/message-icons/no-categorie-products.png')} style={styles.noProductImage} />
-                    <Text style={styles.noProductText} >Kein Produkt gefunden</Text>
-                </View>
-            )
-        }
-        return (
-            <View>
-                <FlatList
-                    // contentContainerStyle={{paddingLeft: 18}}                    
-                    data={this.state.data.filter(item => item.stock > 0)}
-                    renderItem={({ item }) => {
-                        const { companyPrice, previewImgURL, price, productName, productSalePercent, rate, salePrice, stock, productID } = item
-                        return (
-                            <View style={{ paddingBottom: 8 }}>
-                                <ProductListItem
-                                    name={productName}
-                                    //price={price}
-                                    price={this.props.userInfo.selectedUserType === 'EK' ? price.toFixed(2) : companyPrice.toFixed(2)}
-                                    salePrice={salePrice != 0 ? 'UVP ' + salePrice.toFixed(2) : ''}
-                                    companyPrice={companyPrice.toFixed(2)}
-                                    rate={rate}
-                                    stock={stock}
-                                    id={productID}
-                                    imageURL={previewImgURL}
-                                    salePercent={productSalePercent ? productSalePercent.int : null}
-                                />
-                            </View>
-                        )
-                        // }
-                    }}
-                    columnWrapperStyle={{ flexWrap: 'wrap' }}
-                    numColumns={4}
-                    ListFooterComponent={this.state.IDs.length > this.state.data.length && (this.state.from + 12 === this.state.data.length) ? <FooterButton text='Weitere Produkte' onPress={() => { this.getData(this.state.from + 12) }} /> : null}
-                    // ListFooterComponent={<TouchableOpacity onPress={() => {this.getData(this.state.from+12)}} ><Text>END</Text></TouchableOpacity>}
-                    initialNumToRender={12}
-                    windowSize={4}
-                    keyExtractor={item => item.siteURL}
-                />
-            </View>
-        )
+  render() {
+    console.log("this.state RENDER() in products-by-category.js", this.state);
+    if (!this.state.loaded) {
+      return <Loading />
     }
+    if (this.state.IDs.length < 1) {
+      return (
+        <View style={styles.noProductsContainer}>
+          <Image source={require('../../assets/message-icons/no-categorie-products.png')} style={styles.noProductImage} />
+          <Text style={styles.noProductText} >Kein Produkt gefunden</Text>
+        </View>
+      );
+    }
+    return (
+      <View style={s.container}>
+        <View>
+          <FlatList
+            // contentContainerStyle={{paddingLeft: 18}}                    
+            data={this.state.data.filter(item => item.stock > 0)}
+            renderItem={({ item }) => {
+              const { companyPrice, previewImgURL, price, productName, productSalePercent, rate, salePrice, stock, productID } = item;
+              return (
+                <View style={{ paddingBottom: 8 }}>
+                  <ProductListItem
+                    name={productName}
+                    //price={price}
+                    price={this.props.userInfo.selectedUserType === 'EK' ? price.toFixed(2) : companyPrice.toFixed(2)}
+                    salePrice={salePrice != 0 ? 'UVP ' + salePrice.toFixed(2) : ''}
+                    companyPrice={companyPrice.toFixed(2)}
+                    rate={rate}
+                    stock={stock}
+                    id={productID}
+                    imageURL={previewImgURL}
+                    salePercent={productSalePercent ? productSalePercent.int : null}
+                  />
+                </View>
+              );
+            }}
+            columnWrapperStyle={{ flexWrap: 'wrap' }}
+            numColumns={4}
+            ListFooterComponent={this.state.IDs.length > this.state.data.length && (this.state.from + 12 === this.state.data.length) ? <FooterButton text='Weitere Produkte' onPress={() => { this.getData(this.state.from + 12) }} /> : null}
+            // ListFooterComponent={<TouchableOpacity onPress={() => {this.getData(this.state.from+12)}} ><Text>END</Text></TouchableOpacity>}
+            initialNumToRender={12}
+            windowSize={4}
+            keyExtractor={item => item.siteURL}
+          />
+        </View>
+        <View style={s.footer}>
+          <FooterNavBar />
+        </View>
+      </View>
+    );
+  }
 }
 
+const s = {
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  footer: { width: '100%' },
+};
 
 const mapStateToProps = ({ userID, userInfo }) => ({ userID, userInfo })
 
